9fa99caa10ce2d3c4d08fbbb5ef326841afb7f0b
sze <sze.sas1465pt@gmail.com>
2018-12-09 20:09:26 +0900

    lesson6 課題 発展課題

---
+++ Assets/ObjectDestroyer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour {
+
+    //UnityChanを入れる
+    private GameObject unitychan;
+
+
+    // Use this for initialization
+    void Start () {
+
+        //unityちゃんのgameobjectをとる
+        unitychan = GameObject.Find("unitychan");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //unitychanより15m後ろの位置になったら
+        if (this.transform.position.z < unitychan.transform.position.z - 5)
+        {
+            //GameObjectを破壊
+            Destroy(this.gameObject);
+        }
+
+    }
+}
